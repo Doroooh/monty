34f12bcad3759ed509fe961a9dfbcd756f2aa6ce
Waithira <dorotttyyy@gmail.com>
2024-03-28 17:10:56 -0700

    monty

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - It does nothing.
+ * @stack: Pointer to the  pointer pointing to the top node of my stack.
+ * @line_number: the integer representing the line number of opcode.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
